185673e2a1895cec61d4c3485ea8b6a5b5dcb94a
zeeshan_asgar <asgarzeeshan@gmail.com>
2016-10-24 15:50:01 +0530

    initial commit

--- ocrapp/models.py
+++ ocrapp/models.py
@@ -1,11 +1,10 @@
-from django.db import models
 import os
 import uuid
 
+from django.db import models
 
-# Create your models here.
+
 def upload(instance, filename):
-    # ext = filename.split('.')[-1]
     filename = str(uuid.uuid4())
     return os.path.join('pictures', filename)
 
